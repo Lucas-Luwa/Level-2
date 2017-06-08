f324ba7dd82ffa220b2cc897815b42528156b4e3
DavidThorpe123 <league@WorthyMini-6.attlocal.net>
2017-06-07 18:32:41 -0700

    JO
    JO

---
+++ src/PACMAN/Starter.java
@@ -0,0 +1,20 @@
+package PACMAN;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Starter {
+	public static void main(String[] args) {
+		Starter S = new Starter();
+		S.Create();
+	}
+
+	void Create() {
+		JFrame f = new JFrame();
+		JPanel p = new JPanel();
+		f.add(p);
+		f.setVisible(true);
+		f.setSize(1000, 1000);
+	
+	}
+}
